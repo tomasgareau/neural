fff7ff3a25a24e332bb2f0e1183c78265ff23e11
Tomas Gareau <tomas@gotchi.local>
2016-10-31 10:26:58 -0400

    Initial Commit

---
+++ neural/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  neural
+//
+//  Created by Tomas Gareau on 2016-10-31.
+//  Copyright © 2016 Tomas Gareau. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
